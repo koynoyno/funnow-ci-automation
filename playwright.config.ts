9f9b39ab57b98a848e006261afe0dfe4a6f83790
Vlad Volkov <vlkv@tuta.io>
2024-01-31 11:44:31 +0800

    add .slow() option to the booking test

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,8 +12,9 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
   reporter: process.env.CI ? [
+    ['list']
     ['github'],
     ['junit', testRailOptions]
   ] : [['list'], ['junit', testRailOptions]],
