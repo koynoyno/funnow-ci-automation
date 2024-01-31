bddca4d7ce11006b020f01e31bddb2363372a5ac
Vlad <vlkv@tuta.io>
2024-01-31 22:39:04 +0700

    Update booking.spec.ts

--- tests/booking.spec.ts
+++ tests/booking.spec.ts
@@ -1,7 +1,7 @@
 /*
   Note: booking flow (happy path) 
   Tests is parameterized, to test the booking flow in different locales.
-  Tests is using page objects to adhere for SOLID principles.
+  Tests is using page objects to adhere to SOLID principles.
 */
 
 import { test, expect } from '../fixtures/autoFixtures';
