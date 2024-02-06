d0f88143fe7a713e5f04d7dce05727e9d2dadb4e
Vlad <vlkv@tuta.io>
2024-02-06 13:20:09 +0700

    Update booking.spec.ts

--- playwright-typescript/tests/booking.spec.ts
+++ playwright-typescript/tests/booking.spec.ts
@@ -35,9 +35,7 @@ for (const locale of locales) {
       await branchPage.selectNearestBookingTime();
       await expect(branchPage.bookingTimeDialog).not.toBeVisible()
       await branchPage.gotoBookingPaymentPage();
-      await expect(page, `test might fail here because of the bug
-      See https://github.com/koynoyno/funnow-ci-automation/?tab=readme-ov-file#why-are-booking-flow-tests-flaky`)
-        .toHaveURL(/.*booking/);
+      await expect(page).toHaveURL(/.*booking/);
 
       const bookingPaymentPage = new BookingPayment(page, locale);
       await bookingPaymentPage.fillSpecialRequest(SPECIAL_REQUEST);
